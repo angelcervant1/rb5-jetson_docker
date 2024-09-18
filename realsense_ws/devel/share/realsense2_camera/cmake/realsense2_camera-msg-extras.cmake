d3601d175881411fba7c2be2c786cfbc91ab3361
angelcervant1 <angelcervant@hotmail.com>
2024-09-18 19:55:40 +0000

    initial commit

--- realsense_ws/devel/share/realsense2_camera/cmake/realsense2_camera-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(realsense2_camera_MESSAGE_FILES "/ros_ws/realsense_ws/src/realsense-ros/realsense2_camera/msg/IMUInfo.msg;/ros_ws/realsense_ws/src/realsense-ros/realsense2_camera/msg/Extrinsics.msg;/ros_ws/realsense_ws/src/realsense-ros/realsense2_camera/msg/Metadata.msg")
-set(realsense2_camera_SERVICE_FILES "/ros_ws/realsense_ws/src/realsense-ros/realsense2_camera/srv/DeviceInfo.srv")
